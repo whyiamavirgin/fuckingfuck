85ec6c8d80d928133746aac5b6f155d2b864d2b2
whyiamavirgin <izmajlovtima@gmail.com>
2020-11-06 20:11:45 +0600

    правки в двух файлов

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// toggle.onclick = function myFunc() {
-//     var x = document.getElementById('logo');
-//     var y = document.getElementById('menu-checkbox');
-
-//     if (y.is(":checked")){
-//         alert("ON");
-//     } else {
-//         alert("OFF");
-//     }
-
-//     // if (x.className === "logo" && ) {
-//     //     x.className += " invisible";
-//     // } else {
-//     //     x.className = "topnav";
-//     // }
-// }
-
 $('#menu-checkbox').on('click', function () {
     var x = document.getElementById('logo');
     if ( $(this).is(':checked') ) {
