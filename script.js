3bd04afc6746427e7d6e475f95da85502ad213cf
whyiamavirgin <izmajlovtima@gmail.com>
2020-11-08 16:14:10 +0600

    sixth commit

--- script.js
+++ script.js
@@ -21,3 +21,66 @@ $(function() {
         $('.post').show(500);
     });
 });
+
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+      items:8,
+      loop:true,
+      smartSpeed:500,
+      dots:false,
+      autoplay:true,
+      responsiveClass: true,
+      responsive:{
+        0: {
+            items: 1,
+               loop: true,
+               dots: false,
+               smartSpeed: 1000,
+               margin: 5
+        },
+        300: {
+            items:2,
+               loop: true,
+               dots: false,
+               smartSpeed: 1000,
+               margin: 5
+        },
+           420: {
+               items:4,
+               loop: true,
+               dots: false,
+               smartSpeed: 1000,
+               margin: 10
+           },
+           720: {
+               items:6,
+               loop: true,
+               dots: false,
+               smartSpeed: 1000,
+               margin: 15
+           },
+           1100: {
+               items:8,
+        loop: true,
+        dots: false,
+        smartSpeed: 1000,
+        margin: 25
+           }
+       }
+  });
+// if($(window).width() < 420 ) {
+//     owl.owlCarousel({
+//         items:3
+//     });
+// }
+  // Go to the next item
+  $('.owl-next').click(function() {
+      owl.trigger('next.owl.carousel');
+  });
+  // Go to the previous item
+  $('.owl-prev').click(function() {
+      // With optional speed parameter
+      // Parameters has to be in square bracket '[]'
+      owl.trigger('prev.owl.carousel', [300]);
+  });
+
