298e5d1e8a96dff6978fc7bd329aa99cf2a0482b
whyiamavirgin <timofejadamia4@gmail.com>
2020-11-10 01:19:09 +0600

    seventh commit

--- script.js
+++ script.js
@@ -84,3 +84,22 @@ $(function() {
       owl.trigger('prev.owl.carousel', [300]);
   });
 
+
+var abc = document.getElementById('footer-navig');
+if($(window).width() < 420) {
+    abc.className += " disabled";
+} else {
+    abc.className = "footer-nav";
+};
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth", 
+            block: "start"
+        })
+    })
+}
